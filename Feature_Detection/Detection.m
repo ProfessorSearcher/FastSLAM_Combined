bed1988cd6acdaa72dd9f37ab6e09077a9685683
ProfessorSearcher <liuxuechen1994@live.com>
2015-12-07 13:40:29 +0000

    ""

--- Feature_Detection/Detection.m
+++ Feature_Detection/Detection.m
@@ -1,30 +1,29 @@
-cap = cv.VideoCapture('MOV_0017.mp4');
+%Detection of 3D landmarks using FAST corner detection and SFM
+%Xuechen Liu 2015
 
-detector = cv.FeatureDetector('ORB');
-extractor = cv.DescriptorExtractor('ORB');
+video = VideoReader('testing1.mp4');
 
-while(1),
-    flag1, img1 = cap.read();
-    img1 = imresize(img1, 0.25);
-    gr1 = cv.cvtColor(img1, 'RGB2GRAY');
-    flag2, img2 = cap.read();
-    img2 = imresize(img2, 0.25);
-    gr2 = cv.cvtColor(img2, 'RGB2GRAY');
-    
-    keypoints1 = detector.detect(gr1);
-    descriptor1 = extractor.compute(gr1, keypoints1);
-    keypoints2 = detector.detect(gr2);
-    descriptor2 = extractor.compute(gr2, keypoints2);
-    
-    
+%Load camera parameters
+load upToScaleReconstructionCameraParameters.mat
 
-    %pnts = cv.FAST(gr);
-    %xx = zeros(2,length(pnts));
-%        for i=1:50
-%           xx(:,i) = pnts(i).pt;
-%        end
-%     hold on
-%     plot(xx(1,:),xx(2,:),'r.')
+while(hasFrame(video)),
     
-    pause(0.05);
+    %Grab two consecutive frames from one video
+    frame1 = readFrame(video);
+    frame2 = readFrame(video);
+    frame1 = undistortImage(frame1, cameraParams);
+    frame2 = undistortImage(frame2, cameraParams);
+    
+    figure;imshow(rgb2gray(frame1));figure(2);imshow(rgb2gray(frame2));
+    
+    %Extract and match features using FAST
+    points1 = detectFASTFeatures(rgb2gray(frame1));
+    points2 = detectFASTFeatures(rgb2gray(frame2));
+    features1, valid_points1 = extractFeatures(rgb2gray(frame1), points1);
+    features2, valid_points2 = extractFeatures(rgb2gray(frame2), points2);
+    pairs = matchFeatures(features1, features2);
+    
+    %Apply SFM of Multiple Views to get 3D location of landmarks
+    
+    
 end
